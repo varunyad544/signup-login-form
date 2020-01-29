ede235844a5bfd9dc527776ff86eb4ae58772017
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-01-29 18:31:58 +0530

    added signout button

--- welcome.php
+++ welcome.php
@@ -8,5 +8,8 @@ include 'login.php';
 </head>
 <body>
 <h1>Welcome <?php echo $_SESSION["username"] ?> </h1>
+  <button type="button" onclick="signout();">Sign out</button>
+  
+  <script src="script.js"></script>
 </body>
 </html>
