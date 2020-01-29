3bce58f24fb01c613b9ef8981f9d62764d0166d9
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-01-29 11:35:48 +0530

    added email availablity check

--- processing.php
+++ processing.php
@@ -10,28 +10,43 @@ $phone = $_POST["phone"];
 $email = $_POST["email"];
 $pass = $_POST["pass"];
 
+$servername = "34.66.9.69";
+$dbusername = "varun1";
+$dbpassword = "Varun@123";
+$dbname = "mydb";
+
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
 if($username!="" and $phone!="" and $email!="" and $pass!=""){
-	$servername = "34.66.9.69";
-	$dbusername = "varun1";
-	$dbpassword = "Varun@123";
-	$dbname = "mydb";
+	emailAvailable = checkEmailAvailability($email);
+	
+	if(emailAvailable){
+		$sql = "INSERT INTO users(username, phone, email, password) VALUES ('$username','$phone','$email','$pass')";
 
-	$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
-
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-
-	$sql = "INSERT INTO users(username, phone, email, password) VALUES ('$username','$phone','$email','$pass')";
-
-	if ($conn->query($sql) === TRUE){
-		echo 1;
+		if ($conn->query($sql) === TRUE){
+			echo 1;
+		}else{
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}	
 	}else{
-		echo "Error: " . $sql . "<br>" . $conn->error;
+		echo "email already exists.";
 	}
 	$conn->close();
 }
 else{
 	echo 0;	
+}
+
+function checkEmailAvailability($email){
+	$result = $conn->query("SELECT user_id from users where email = '$email'");	
+	if($result->num_rows == 0){
+		return true;	
+	}else{
+		return false;	
+	}
 }
 ?>
