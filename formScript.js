81f08bc5683aeb96c97308ad243fa432050020bb
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-01-30 18:34:37 +0530

    Update formScript.js

--- formScript.js
+++ formScript.js
@@ -177,9 +177,7 @@ function login(){
 				pass: pass},
 			success: function(response){
 				response = JSON.parse(response);
-				console.log(response.allUserData);
 				if(response.validUser == true && response.isAdmin == true){
-					console.log(response.validUser, response.isAdmin);
 					renderAllUsersData(response.allUserData);
 					window.location.replace("http://34.66.9.69/signup-login-form/homepage.php");		
 				}else if(response.validUser == true && response.isAdmin == false){
