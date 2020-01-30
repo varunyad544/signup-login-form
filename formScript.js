31e3b7ad9387eb4639ee84058c0678325c957e25
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-01-30 16:34:21 +0530

    Update formScript.js

--- formScript.js
+++ formScript.js
@@ -177,7 +177,7 @@ function login(){
 				pass: pass},
 			success: function(response){
 				console.log('response = ' + response);
-				window.location.replace("http://34.66.9.69/signup-login-form/welcome.php");	
+				window.location.replace("http://34.66.9.69/signup-login-form/homepage.php");	
 			},
 			error: function(error){
 				console.log('error: ' +error);
