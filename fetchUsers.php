e0bc315ec9ae46d932a09fafe9b38ae4e3a19502
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-04 15:30:17 +0530

    Update fetchUsers.php

--- fetchUsers.php
+++ fetchUsers.php
@@ -1,11 +1,22 @@
 <?php
-	$userData = array();
-	$sql = "SELECT * FROM users WHERE username!='admin'";
-	$result = $conn->query($sql);
-	
-	while($data = $result->fetch_assoc()){
-		$userData += [$data['user_id']=> array('username'=>$data['username'], 'phone'=>$data['phone'], 
-		      					'email'=>$data['email'], 'password'=>$data['password'])];
-	}
-	die(json_encode($userData));
+$servername = "34.66.9.69";
+$dbusername = "varun1";
+$dbpassword = "Varun@123";
+$dbname = "mydb";
+
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$userData = array();
+$sql = "SELECT * FROM users WHERE username!='admin'";
+$result = $conn->query($sql);
+
+while($data = $result->fetch_assoc()){
+	$userData += [$data['user_id']=> array('username'=>$data['username'], 'phone'=>$data['phone'], 
+						'email'=>$data['email'], 'password'=>$data['password'])];
+}
+die(json_encode($userData));
 ?>
