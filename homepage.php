ef6e36edf08d756f1e12828b270208919b5685ff
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-04 15:22:23 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -5,6 +5,26 @@ include 'login.php';
 <html>
 <head>
 	<link rel="shortcut icon" href="#" />
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	
+</head>
+<body>
+	<h1>Welcome <?php echo $_SESSION["username"] ?> </h1>
+  	<p>
+	 
+  	</p>
+  	<button type="button" onclick="signout();">Sign out</button>
+  	<div id="data">
+    
+  	</div>
+	<script src="homepageScript.js"></script>
+	
 	<script>
 		var id = <?php echo $_SESSION["userId"] ?>;
 		if(id==13){
@@ -21,16 +41,5 @@ include 'login.php';
 		});	
 		}
 	</script>
-</head>
-<body>
-	<h1>Welcome <?php echo $_SESSION["username"] ?> </h1>
-  	<p>
-	 
-  	</p>
-  	<button type="button" onclick="signout();">Sign out</button>
-  	<div id="data">
-    
-  	</div>
-	<script src="homepageScript.js"></script>
 </body>
 </html>
