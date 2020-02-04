4d3f569f3c8971eec07a627566a1bce8d8623a66
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-04 12:33:55 +0530

    Update homepageScript.js

--- homepageScript.js
+++ homepageScript.js
@@ -2,7 +2,7 @@ function renderAllUsersData(data){
 	console.log(data);
 	var userTable = "<table style='width:100%'><tr><th>User ID</th><th>Username</th><th>Phone</th><th>Email</th></tr>";
 	for(key in data){
-		console.log(key);
+		//console.log(key);
 		//userTable+= "<tr><td>" + key + "</td><td>" + data.key.username + "</td><td>" + data.key.phone + 
 			//"</td><td>" + data.key.email + "</td></tr>";
 	}
