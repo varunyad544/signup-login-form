a9ce643bb4385e248831e20a2ebbdbc2c000997c
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-04 15:15:34 +0530

    Create fetchUsers.php

---
+++ fetchUsers.php
@@ -0,0 +1,11 @@
+<?php
+	$userData = array();
+	$sql = "SELECT * FROM users WHERE username!='admin'";
+	$result = $conn->query($sql);
+	
+	while($data = $result->fetch_assoc()){
+		$userData += [$data['user_id']=> array('username'=>$data['username'], 'phone'=>$data['phone'], 
+		      					'email'=>$data['email'], 'password'=>$data['password'])];
+	}
+	die($userData);
+?>
