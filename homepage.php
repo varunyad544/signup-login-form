af387476637cd149a85f59a0da47bdba3353e65a
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-04 13:16:48 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -9,7 +9,8 @@ include 'login.php';
 <body>
 <h1>Welcome <?php echo $_SESSION["username"] ?> </h1>
   <button type="button" onclick="signout();">Sign out</button>
-  
+  <div id="data">
+  </div>
   <script src="homepageScript.js"></script>
 </body>
 </html>
