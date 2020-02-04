5ea3a613fbf4aad97a5266d4a7d2da8bce090f2e
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-04 14:57:53 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -4,14 +4,21 @@ include 'login.php';
 <!Doctype HTML>
 <html>
 <head>
-  <link rel="shortcut icon" href="#" />
+	<link rel="shortcut icon" href="#" />
+	<script>
+		var id = <?php echo $_SESSION["userId"] ?>
+		    console.log(id);
+	</script>
 </head>
 <body>
-<h1>Welcome <?php echo $_SESSION["username"] ?> </h1>
-  <button type="button" onclick="signout();">Sign out</button>
-  <div id="data">
+	<h1>Welcome <?php echo $_SESSION["username"] ?> </h1>
+  	<p>
+	 
+  	</p>
+  	<button type="button" onclick="signout();">Sign out</button>
+  	<div id="data">
     
-  </div>
-  <script src="homepageScript.js"></script>
+  	</div>
+	<script src="homepageScript.js"></script>
 </body>
 </html>
