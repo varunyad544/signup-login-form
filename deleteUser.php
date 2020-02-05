9c5dfa01f1483fa8f3a8745ec7c6b232b5fee74d
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-05 12:22:56 +0530

    Create deleteUser.php

---
+++ deleteUser.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "34.66.9.69";
+$dbusername = "varun1";
+$dbpassword = "Varun@123";
+$dbname = "mydb";
+
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$userId = $_POST["userId"];
+
+if($userId!=""){
+	$sql = "DELETE FROM users WHERE	user_id='$userId'";
+	if ($conn->query($sql) === TRUE) {
+		echo 1;
+	} else {
+    		
+	}
+} 
+
+?>
