e45440bd637a4aa5db0cac5cfade7eeb54ef7461
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-05 11:23:34 +0530

    Update homepageScript.js

--- homepageScript.js
+++ homepageScript.js
@@ -2,9 +2,13 @@ function renderAllUsersData(data){
 	var userTable = "<table><tr><th>User ID</th><th>Username</th><th>Phone</th><th>Email</th><th></th></tr>";
 	for(key in data){
 		userTable+= "<tr><td>" + key + "</td><td>" + data[key]['username'] + "</td><td>" + data[key]['phone'] + 
-			"</td><td>" + data[key]['email'] + "</td><td><button class='btn btn-primary edit-btn' onclick='editUser(this);'"+
-			"value='Edit'>Edit</button><button type='button' class='btn btn-danger' data-toggle='modal' data-target='#myModal'>"+
-			"Delete</button></td></tr>";
+			"</td><td>" + data[key]['email'] + "</td><td>";
+		
+		userTable+= "<button type='button' class='btn btn-primary edit-btn' onclick='editUser(this);' value='Edit'>Edit</button>";
+		
+		userTable+= "<button type='button' class='btn btn-danger' data-toggle='modal' data-target='#myModal'>Delete</button>";
+		
+		userTable+= "</td></tr>";
 	}
 	userTable+= "</table>";
 	$('#data').append(userTable);
@@ -14,6 +18,7 @@ function signout(){
 	window.location.replace("http://34.66.9.69/signup-login-form/index.php");
 }
 
-function deleteUser(e){
+function deleteUser(){
+	console.log(this);
 	var row = $(this).parent().parent().remove();
 }
