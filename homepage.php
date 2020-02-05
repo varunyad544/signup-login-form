9c19c1b0b8fe1e591266d13887cd5d9fc48dd6e1
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-05 11:16:29 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -25,6 +25,31 @@ include 'login.php';
   	<div id="data">
     
   	</div>
+	
+	<div class="modal" id="myModal">
+		<div class="modal-dialog">
+			<div class="modal-content">
+
+				<!-- Modal Header -->
+				<div class="modal-header">
+				<h4 class="modal-title">Delete Account?</h4>
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				</div>
+
+				<!-- Modal body -->
+				<div class="modal-body">
+				Are you sure to delete this user permanentaly?
+					<button type='button' class='btn btn-danger delete-btn' onclick="deleteUser();">Delete</button>
+				</div>
+
+				<!-- Modal footer -->
+				<div class="modal-footer">
+				<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+				</div>
+
+			</div>
+		</div>
+	</div>
 	<script src="homepageScript.js"></script>
 	
 	<script>
