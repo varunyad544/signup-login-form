18ae758fd65baf33a4137711b0ac24248efaf31f
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-05 17:07:06 +0530

    ajax to update user added

--- homepageScript.js
+++ homepageScript.js
@@ -35,11 +35,36 @@ function editUser(e){
 		}
 	}else if($(e).val()=='Save'){
 		$(e).val('Edit');
-		$(e).html('Save');
+		$(e).html('Edit');
 		for(var j=0;j<3;j++){
 			$("#"+editUserRowId).children().children()[j].readOnly = true; 
-			$("#"+editUserRowId).children().children()[j].style.border = "none";	
+			$("#"+editUserRowId).children().children()[j].style.border = "none";
 		}
+		var newUsername = $("#"+editUserRowId).children().children()[0].value;
+		var newPhone = $("#"+editUserRowId).children().children()[1].value;
+		var newEmail = $("#"+editUserRowId).children().children()[2].value;
+		var editUserId = $("#"+editUserRowId).children()[0].innerText;
+		
+		$.ajax({
+		type: 'POST',
+		async: false,
+		url: 'http://34.66.9.69/signup-login-form/updateUser.php',
+		data: {
+			userId: editUserId,
+			username: newUsername,
+			phone: newPhone,
+			email: newEmail
+		},
+		success: function(response){
+			if(response==1){
+				console.log('user updated');
+			}
+		},
+		error: function(error){
+			console.log(error);
+		}
+	});
+		
 	}else{
 		console.log('error');	
 	}
@@ -51,7 +76,7 @@ function setModal(e){
 	
 	var x = $("#"+deleteUserRowId).find("input");
 	deleteUsername = x[0]['value'];
-	$('.modal-title').text('Delete '+ deleteUsername);
+	$('.modal-title').text('Delete '+ deleteUsername + '?');
 }
 
 function deleteUser(){
