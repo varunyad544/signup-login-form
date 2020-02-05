006b20b7778ef7bb15e15aa49399444915b5d987
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-05 11:38:50 +0530

    Update homepageScript.js

--- homepageScript.js
+++ homepageScript.js
@@ -1,12 +1,16 @@
-function renderAllUsersData(data){
+$(document).ready(function(){
+	var deleteUserRowId='';
+	var deleteUsername='';
+	
+	function renderAllUsersData(data){
 	var userTable = "<table><tr><th>User ID</th><th>Username</th><th>Phone</th><th>Email</th><th></th></tr>";
 	for(key in data){
-		userTable+= "<tr><td>" + key + "</td><td>" + data[key]['username'] + "</td><td>" + data[key]['phone'] + 
+		userTable+= "<tr id='row"+key+"'><td>" + key + "</td><td>" + data[key]['username'] + "</td><td>" + data[key]['phone'] + 
 			"</td><td>" + data[key]['email'] + "</td><td>";
 		
 		userTable+= "<button type='button' class='btn btn-primary edit-btn' onclick='editUser(this);' value='Edit'>Edit</button>";
 		
-		userTable+= "<button type='button' class='btn btn-danger' data-toggle='modal' data-target='#myModal'>Delete</button>";
+		userTable+= "<button type='button' class='btn btn-danger' onclick='setModal(this);' data-toggle='modal' data-target='#myModal'>Delete</button>";
 		
 		userTable+= "</td></tr>";
 	}
@@ -18,7 +22,12 @@ function signout(){
 	window.location.replace("http://34.66.9.69/signup-login-form/index.php");
 }
 
-function deleteUser(){
-	console.log(this);
-	var row = $(this).parent().parent().remove();
+function setModal(e){
+	deleteUserRowId = $(e).parent().parent().attr('id');
+	console.log(deleteUserRowId);
 }
+
+function deleteUser(){
+}
+	
+});
