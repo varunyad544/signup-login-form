40fadcf1f4287078b44631941757def7392c7670
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-05 15:18:59 +0530

    Update homepageScript.js

--- homepageScript.js
+++ homepageScript.js
@@ -32,7 +32,7 @@ function editUser(button){
 
 function setModal(e){
 	deleteUserRowId = $(e).parent().parent().attr('id');
-	console.log($("#"+deleteUserRowId).children()[0]);
+	console.log($("#"+deleteUserRowId).children()[0].innerText);
 	var x = $("#"+deleteUserRowId).find("input");
 	deleteUsername = x[0]['value'];
 	$('.modal-title').text('Delete '+ deleteUsername);
