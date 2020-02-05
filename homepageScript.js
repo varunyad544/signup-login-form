e4fadd3f65066d85c40084fc07f6f5a33d7338fd
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-05 11:16:49 +0530

    Update homepageScript.js

--- homepageScript.js
+++ homepageScript.js
@@ -2,8 +2,8 @@ function renderAllUsersData(data){
 	var userTable = "<table><tr><th>User ID</th><th>Username</th><th>Phone</th><th>Email</th><th></th></tr>";
 	for(key in data){
 		userTable+= "<tr><td>" + key + "</td><td>" + data[key]['username'] + "</td><td>" + data[key]['phone'] + 
-			"</td><td>" + data[key]['email'] + "</td><td><button class='edit-btn' onclick='editUser' value='Edit'>Edit</button>"+
-			"<button type='button' class='btn btn-danger delete-btn' onclick='deleteUser' value='Delete'>Delete</button></td></tr>";
+			"</td><td>" + data[key]['email'] + "</td><td><button class='edit-btn' onclick='editUser(this);' value='Edit'>Edit</button>"+
+			"<button type='button' class='btn btn-danger' data-toggle='modal' data-target='#myModal'>Delete</button></td></tr>";
 	}
 	userTable+= "</table>";
 	$('#data').append(userTable);
@@ -11,4 +11,8 @@ function renderAllUsersData(data){
 
 function signout(){
 	window.location.replace("http://34.66.9.69/signup-login-form/index.php");
+}
+
+function deleteUsers(e){
+	var row = $(this).parent().parent().remove();
 }
