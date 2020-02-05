fb4c7fe85b50732e25d3ee64649a22ff96a56d94
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-05 11:56:01 +0530

    Update homepageScript.js

--- homepageScript.js
+++ homepageScript.js
@@ -26,7 +26,7 @@ function setModal(e){
 	var x = $("#"+deleteUserRowId).children();
 	console.log(x);
 	console.log(x[0]);
-	console.log(x[0].text);
+	console.log(x[0].textContent);
 }
 
 function deleteUser(){
