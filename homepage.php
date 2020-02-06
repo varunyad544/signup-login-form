56663a9e0787bead97de33ef3ad58b9a112c014e
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-02-06 10:40:25 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-echo " <script> console.log(session variable check: '" + $_SESSION['username'] + "'); </script> ";
+echo " <script> console.log('" + $_SESSION["username"] + "') </script> ";
 if(!isset($_SESSION['username'])){
 	header("Location: http://34.66.9.69/signup-login-form/index.php");
 }
