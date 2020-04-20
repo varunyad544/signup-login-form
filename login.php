1b9974c97983675c98886ced2727a2900a5a3b75
varunyadav <33948167+varunyad544@users.noreply.github.com>
2020-04-21 00:15:43 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 
 session_start();
-$servername = "34.66.9.69";
-$dbusername = "varun1";
+$servername = "104.154.144.118";
+$dbusername = "root";
 $dbpassword = "Varun@123";
 $dbname = "mydb";
 
